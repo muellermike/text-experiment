9b57fde8288b6d340381cb29ce36b1cd341e5d56
muellermike <mueller.doublem@gmail.com>
2022-10-08 07:34:05 +0200

    define storeExpName

--- src/components/ParticipantIdentifier/ParticipantIdentifier.js
+++ src/components/ParticipantIdentifier/ParticipantIdentifier.js
@@ -2,7 +2,7 @@ import { useState, React, useEffect } from "react";
 import { Button, Card } from "react-bootstrap";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useDispatch } from 'react-redux';
-import { storeExternalUserId, storeUserId, storeExperimentId, storeImageTime } from '../../actions';
+import { storeExternalUserId, storeUserId, storeExperimentId, storeImageTime, storeExpName } from '../../actions';
 
 function ParticipantIdentifier() {
 
